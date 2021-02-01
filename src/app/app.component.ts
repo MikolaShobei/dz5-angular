b26017b29e90885c55b65a17b8642b16a597f29e
MikolaShobei <mikola.shobey.wr@gmail.com>
2021-02-01 17:53:06 +0200

    qwert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, NgForm} from '@angular/forms';
+import {UserCreate} from './creators/user-create';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'dz5-angular';
+  name = new FormControl('name');
+  age = new FormControl(45);
+  myForm1 = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+  usersList = [];
+  user = {name: '', age: null};
+  usersList2 =  [];
+
+  myEvent(): void {
+    console.log(this.myForm1);
+    const newUser = new UserCreate(this.myForm1.controls.name.value, this.myForm1.controls.age.value);
+    this.usersList.push(newUser);
+    console.log(this.usersList);
+  }
+
+  nyForm2Event(myForm2: NgForm): void {
+    console.log(myForm2, 'myForm2');
+    console.log(myForm2.form.controls.name.value);
+    this.usersList2.push({name: myForm2.form.controls.name.value, age: myForm2.form.controls.age.value});
+    console.log(this.usersList2);
+  }
 }
+
+
+
+
+
+
---
+++ src/app/creators/user-create.ts
@@ -0,0 +1,6 @@
+export class UserCreate {
+   constructor(private name: string, private age: number) {
+     this.name = name;
+     this.age = age;
+   }
+  }
